bd1a22e87ed24c209b2cf8bd4f79f1cc4d513ece
Tibor Arpas <tibor.arpas@infinit.sk>
2015-02-23 20:05:47 +0100

    The setuptools-markdown requirement would be likely to cause problems, it's much better to just stick with rst . closes #1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='testmon',
     description='Test Runner for Python',
-    version='0.1.1a2',
+    version='0.1.1a4',
     license='MIT',
     platforms=['linux', 'osx', 'win32'],
     packages=['testmon'],
@@ -11,15 +11,13 @@ setup(
     url='http://igg.me/at/testmon',
     author_email='tibor.arpas@infinit.sk',
     author='Tibor Arpas, Jozef Knaperek, Martin Riesz',
-    data_files=([('', ['README.md']), ]),
+    data_files=([('', ['README.rst']), ]),
     entry_points={
         'pytest11': [
             'testmon = testmon.plugin',
         ]
     },
     install_requires=['pytest<2.7', 'pytest-cache>=1.0', 'watchdog>=0.8'],
-    setup_requires=['setuptools-markdown'],
-    long_description_markdown_filename='README.md',
     classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
