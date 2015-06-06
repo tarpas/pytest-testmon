47c02f0f7defc0a13df5fd6bbaaa5911ac6dd1eb
Tibor Arpas <tibor.arpas@infinit.sk>
2015-06-06 11:30:59 +0200

    norecursedirs=.* (to exclude .git etc)

--- t.py
+++ t.py
@@ -2,4 +2,4 @@ import pytest
 
 if __name__ == '__main__':
     #test/test_testmon.py::TestmonDeselect::test_nonfunc_class
-    pytest.main("--tb=native -v", )
+    pytest.main("--tb=native -v --testmon" )
