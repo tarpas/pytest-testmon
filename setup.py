8b9c0349baa0a71935350d01b3b892c6a353ac85
Daniel Hahler <git@thequod.de>
2017-05-23 15:46:14 +0200

    Add support for pytest 3.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
             'testmon = testmon.pytest_testmon',
         ]
     },
-    install_requires=['pytest>=2.8.0,<3.1', 'coverage>=4'],
+    install_requires=['pytest>=2.8.0,<3.2', 'coverage>=4'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
