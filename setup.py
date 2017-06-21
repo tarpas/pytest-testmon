1b8c9011b9559faa05d74000593a2f6cb8e1a328
Tibor Arpas <tibor.arpas@infinit.sk>
2017-06-21 16:19:33 +0200

    bump to version 0.9.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pytest-testmon',
     description='take TDD to a new level with py.test and testmon',
     long_description=''.join(open('README.rst').readlines()),
-    version='0.9.5',
+    version='0.9.6',
     license='MIT',
     platforms=['linux', 'osx', 'win32'],
     packages=['testmon'],
