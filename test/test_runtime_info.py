ef1d180f3b343db4a7c0a5fae7a8e656c73812b2
Adrian Nagy <drnnagy@gmail.com>
2018-10-02 12:25:28 +0200

    reworked unit test (JSON -> sqlite)

--- test/test_runtime_info.py
+++ test/test_runtime_info.py
@@ -1,7 +1,8 @@
-from runtime_info import pytest_runtime_info
+import pytest_runtime_info
 import os
 import json
 import pytest
+import sqlite3
 
 
 @pytest.fixture
@@ -37,36 +38,49 @@ def b_py(testdir):
 
 
 def test_plugin(testdir, lib_py, a_py, b_py):
-    result_file = pytest_runtime_info.get_temp_file_path()
+    db_file = os.path.join(os.getcwd(), "runtime_test_report.db")
+    conn = sqlite3.connect(db_file)
+
+    c = conn.cursor()
+
     try:
-        file_modified = os.stat(result_file).st_mtime
+        file_modified = os.stat(db_file).st_mtime
     except Exception:
         file_modified = 0
     test_result = testdir.runpytest()
     # test if failed
     test_result.assert_outcomes(failed=2, passed=1)
     # test if result file was modified
-    assert file_modified != os.stat(result_file).st_mtime
-    with open(result_file) as result_stream:
-        result_json = result_stream.read()
-        result = json.loads(result_json)
+    assert file_modified != os.stat(db_file).st_mtime
+
     a_file = os.path.join(str(testdir.tmpdir), "test_a.py")
     b_file = os.path.join(str(testdir.tmpdir), "test_b.py")
     lib_file = os.path.join(str(testdir.tmpdir), "lib.py")
-    assert len(result["exceptions"]) == 2
-    assert len([x for x in result["exceptions"] if x["path"] == lib_file]) == 1
-    assert len([x for x in result["exceptions"] if x["path"] == b_file]) == 1
-    assert len(result["fileMarkList"]) == 3
-    assert len([x for x in result["fileMarkList"] if x["path"] == lib_file]) == 1
-    assert len([x for x in result["fileMarkList"] if x["path"] == b_file]) == 1
-    assert len([x for x in result["fileMarkList"] if x["path"] == a_file]) == 1
-    fileMarksA = [x for x in result["fileMarkList"] if x["path"] == a_file][0]
-    fileMarksB = [x for x in result["fileMarkList"] if x["path"] == b_file][0]
-    fileMarksLib = [x for x in result["fileMarkList"] if x["path"] == lib_file][0]
-    assert len(fileMarksA["marks"]) == 3
-    assert len(fileMarksB["marks"]) == 2
-    assert len(fileMarksLib["marks"]) == 7
-    gutterMarks = [x for x in fileMarksLib["marks"] if x["type"] == "GutterLink"]
+
+    # Exception Table
+    c.execute("SELECT * FROM Exception")
+    result = c.fetchall()
+
+    # test the number of exceptions
+    assert len(result) == 2
+
+    # test the exception paths
+    assert len([x for x in result if x[1] == lib_file]) == 1
+    assert len([x for x in result if x[1] == b_file]) == 1
+
+    # FileMark Table
+    c.execute("SELECT * FROM FileMark")
+    result = c.fetchall()
+
+    # test the number of marks for each file
+    assert len([x for x in result if x[3] == lib_file]) == 7
+    assert len([x for x in result if x[3] == b_file]) == 2
+    assert len([x for x in result if x[3] == a_file]) == 3
+
+    # test the number of GutterLinks for file lib_file
+    gutterMarks = [x for x in result if x[3] == lib_file and x[1] == "GutterLink"]
     assert len(gutterMarks) == 3
-    assert len([x for x in gutterMarks if x["targetPath"] == lib_file]) == 2
-    assert len([x for x in gutterMarks if x["targetPath"] == a_file]) == 1
+
+    # test the target paths for the gutterMarks above
+    assert len([x for x in gutterMarks if x[9] == lib_file]) == 2
+    assert len([x for x in gutterMarks if x[9] == a_file]) == 1
