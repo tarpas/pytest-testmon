8406309cc1d190ed6ddbc3c186e4f212adb4b45a
Aleksandr <aldanchenko@gmail.com>
2018-10-07 16:01:53 +0300

    Add initial version of code to work with Sqlite database. Add initial version of error message display.

---
+++ pycharm/src/sk/infinit/testmon/database/PyFileMark.kt
@@ -0,0 +1,8 @@
+package sk.infinit.testmon.database
+
+/**
+ * Map database FileMark table to object.
+ */
+class PyFileMark(val id: Int, val type: String, val text: String, val fileName: String, val beginLine: Int, val beginCharacter: Int,
+                 val endLine: Int, val endCharacter: Int, val checkContent: String, val targetPath: String,
+                 val targetLine: Int, val targetCharacter: Int, val gutterLinkType: String, val exceptionId: Int)
