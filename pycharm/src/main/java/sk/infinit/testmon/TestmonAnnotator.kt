933583d95a3a305f25db8618e6d6b04062a023ff
Aleksandr <aldanchenko@gmail.com>
2018-11-13 18:06:51 +0200

    Fix react to code change (remove annotations) #35.

--- pycharm/src/main/java/sk/infinit/testmon/TestmonAnnotator.kt
+++ pycharm/src/main/java/sk/infinit/testmon/TestmonAnnotator.kt
@@ -2,12 +2,13 @@ package sk.infinit.testmon
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
-import com.intellij.openapi.editor.*
 import com.intellij.psi.PsiElement
 import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiDocumentManager
 import sk.infinit.testmon.database.DatabaseService
 import java.io.File
-import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.jetbrains.python.psi.PyStatement
 import sk.infinit.testmon.database.FileMarkType
 
 /**
@@ -19,11 +20,9 @@ class TestmonAnnotator : Annotator {
      * Draw underline decorations by Testmon exceptions data.
      */
     override fun annotate(psiElement: PsiElement, annotationHolder: AnnotationHolder) {
-        if (psiElement is PsiFile) {
+        if (psiElement is PyStatement) {
             val project = psiElement.project
             val virtualFile = psiElement.containingFile.virtualFile
-
-            val editor = getEditor(project, psiElement) ?: return
 
             val projectRootVirtualFile = getProjectRootDirectoryVirtualFile(project, virtualFile)
                     ?: return
@@ -34,24 +33,28 @@ class TestmonAnnotator : Annotator {
 
             val pyFileFullPath = projectRootVirtualFile.path + File.separator + virtualFileRelativePath
 
+            val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
+            val document = PsiDocumentManager.getInstance(project).getDocument(psiFile!!)
+
+            val lineNumber = document?.getLineNumber(psiElement.textOffset)
+
             val fileMarks = databaseService
-                    .getFileMarks(pyFileFullPath, null, FileMarkType.RED_UNDERLINE_DECORATION.value)
+                    .getFileMarks(pyFileFullPath, lineNumber, FileMarkType.RED_UNDERLINE_DECORATION.value)
 
             for (fileMark in fileMarks) {
                 try {
-                    val logicalStartPosition = LogicalPosition(fileMark.beginLine, fileMark.beginCharacter)
-                    val logicalEndPosition = LogicalPosition(fileMark.endLine, fileMark.endCharacter)
+                    if (fileMark.checkContent == psiElement.text) {
+                        val startOffset = psiElement.textRange.startOffset
+                        val endOffset = psiElement.textRange.endOffset
 
-                    val startOffset = editor.logicalPositionToOffset(logicalStartPosition)
-                    val endOffset = editor.logicalPositionToOffset(logicalEndPosition)
+                        val range = TextRange(startOffset, endOffset)
 
-                    val range = TextRange(startOffset, endOffset)
+                        val pyException = databaseService.getPyException(fileMark.exceptionId)
 
-                    val pyException = databaseService.getPyException(fileMark.exceptionId)
+                        val annotation = annotationHolder.createErrorAnnotation(range, pyException?.exceptionText)
 
-                    val annotation = annotationHolder.createErrorAnnotation(range, pyException?.exceptionText)
-
-                    annotation.tooltip = pyException?.exceptionText
+                        annotation.tooltip = pyException?.exceptionText
+                    }
                 } catch (exception: Exception) {
                     logErrorMessage(exception)
                 }
--- pycharm/src/main/java/sk/infinit/testmon/TestmonEditorLinePainter.kt
+++ pycharm/src/main/java/sk/infinit/testmon/TestmonEditorLinePainter.kt
@@ -5,6 +5,10 @@ import com.intellij.openapi.editor.LineExtensionInfo
 import com.intellij.openapi.editor.markup.EffectType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiManager
+import com.jetbrains.python.psi.PyStatement
 import sk.infinit.testmon.database.DatabaseService
 import sk.infinit.testmon.database.FileMarkType
 import java.awt.Color
@@ -21,7 +25,8 @@ class TestmonEditorLinePainter : EditorLinePainter() {
      *
      * @return MutableCollection<LineExtensionInfo>
      */
-    override fun getLineExtensions(project: Project, virtualFile: VirtualFile, lineNumber: Int): MutableCollection<LineExtensionInfo> {
+    override fun getLineExtensions(project: Project, virtualFile: VirtualFile, lineNumber: Int)
+            : MutableCollection<LineExtensionInfo> {
         val lineExtensionInfos = mutableListOf<LineExtensionInfo>()
 
         val projectRootVirtualFile = getProjectRootDirectoryVirtualFile(project, virtualFile)
@@ -35,16 +40,41 @@ class TestmonEditorLinePainter : EditorLinePainter() {
         val fileMarks = databaseService
                 .getFileMarks(pyFileFullPath, lineNumber, FileMarkType.RED_UNDERLINE_DECORATION.value)
 
-        for (fileMark in fileMarks) {
-            val lineExtensionInfo = LineExtensionInfo(
-                    "     ${fileMark.text}",
-                    Color.RED,
-                    EffectType.ROUNDED_BOX,
-                    null, Font.PLAIN)
+        val psiElement = getPsiElementAtLine(project, virtualFile, lineNumber)
 
-            lineExtensionInfos.add(lineExtensionInfo)
+        if (psiElement is PyStatement) {
+            for (fileMark in fileMarks) {
+                if (fileMark.checkContent == psiElement.text) {
+                    val lineExtensionInfo = LineExtensionInfo(
+                            "     ${fileMark.text}",
+                            Color.RED,
+                            EffectType.ROUNDED_BOX,
+                            null, Font.PLAIN)
+
+                    lineExtensionInfos.add(lineExtensionInfo)
+                }
+            }
         }
 
         return lineExtensionInfos
+    }
+
+    /**
+     * Get PsiElement by line number.
+     */
+    private fun getPsiElementAtLine(project: Project, virtualFile: VirtualFile, lineNumber: Int)
+            : PsiElement {
+        val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
+
+        val document = PsiDocumentManager.getInstance(project).getDocument(psiFile!!)
+        val offset = document?.getLineStartOffset(lineNumber)
+
+        val psiElement = psiFile.viewProvider.findElementAt(offset!!)
+
+        return if (document.getLineNumber(psiElement!!.textOffset) != lineNumber) {
+            psiElement.nextSibling
+        } else {
+            psiElement
+        }
     }
 }
