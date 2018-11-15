5ce358a8cf54ae20df058409e72135d60d049847
Daniel Hahler <github@thequod.de>
2018-11-15 12:23:51 +0100

    Compatibility with pytest 4 (#118) closes #117
        Remove requirements.txt, rely on setup.py
        requirements: allow pytest<5, pin coverage<5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pytest-testmon',
     description='take TDD to a new level with py.test and testmon',
     long_description=''.join(open('README.rst').readlines()),
-    version='0.9.13',
+    version='0.9.14',
     license='MIT',
     platforms=['linux', 'osx', 'win32'],
     packages=['testmon'],
@@ -19,7 +19,7 @@ setup(
             'testmon = testmon.tox_testmon',
         ],
     },
-    install_requires=['pytest>=2.8.0,<4', 'coverage>=4'],
+    install_requires=['pytest>=2.8.0,<5', 'coverage>=4,<5'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
