5e6d810915eb91047167ba9d3fbea3df793af8bb
Tomáš Matlovič <tomas.matlovic@gmail.com>
2019-03-15 08:11:40 +0100

    Handle multiline filemark content

--- io.intellij/src/main/kotlin/sk/infinit/testmon/extensions/SuffixEditorLinePainter.kt
+++ io.intellij/src/main/kotlin/sk/infinit/testmon/extensions/SuffixEditorLinePainter.kt
@@ -1,12 +1,18 @@
 package sk.infinit.testmon.extensions
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.EditorLinePainter
 import com.intellij.openapi.editor.LineExtensionInfo
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Computable
 import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import sk.infinit.testmon.database.FileMarkType
 import sk.infinit.testmon.database.PyFileMark
 import sk.infinit.testmon.getVirtualFileRelativePath
@@ -36,13 +42,11 @@ class SuffixEditorLinePainter : EditorLinePainter() {
             return lineExtensionInfos
         }
 
-        val line = document.getText(TextRange(
-                document.getLineStartOffset(lineNumber),
-                document.getLineEndOffset(lineNumber)))
+        val pyFileMarks = getPyFileMarks(project, virtualFile)
 
-        val pyFileMarks = getPyFileMarks(project, virtualFile, lineNumber, line)
+        val filteredPyFileMarks = filterPyFileMarks(pyFileMarks, document, lineNumber)
 
-        for (fileMark in pyFileMarks) {
+        for (fileMark in filteredPyFileMarks) {
             lineExtensionInfos.add(LineExtensionInfo(
                     "   ${fileMark.text}",
                     Color.RED,
@@ -56,28 +60,48 @@ class SuffixEditorLinePainter : EditorLinePainter() {
     /**
      * Get file marks from cache or from DB
      */
-    private fun getPyFileMarks(project: Project, virtualFile: VirtualFile, lineNumber: Int, lineText: String):
+    private fun getPyFileMarks(project: Project, virtualFile: VirtualFile):
             List<PyFileMark> {
         val cacheService = ServiceManager.getService(project, Cache::class.java)
                 ?: return ArrayList()
 
         val absoluteFilePath = virtualFile.path
 
-        val fileMarksList = cacheService.getPyFileMarks(absoluteFilePath, FileMarkType.SUFFIX) ?: ArrayList()
+        return cacheService.getPyFileMarks(absoluteFilePath, FileMarkType.SUFFIX) ?: ArrayList()
+    }
 
-        val fileMarks = fileMarksList as MutableList<PyFileMark>
-
-        // Filter by line text.
-        val filteredByLineTextFileMarks = fileMarks.stream()
-                .filter { it.checkContent == lineText }
-                .collect(Collectors.toList())
-
-        // Filter by line number.
-        val filteredByLineNumberFileMarks = filteredByLineTextFileMarks.stream()
+    private fun filterPyFileMarks(fileMarksList: List<PyFileMark>, document: Document, lineNumber: Int): List<PyFileMark> {
+        // Filter by line number
+        val filteredByLineNumberFileMarks = fileMarksList.stream()
                 .filter { it.beginLine == lineNumber }
                 .collect(Collectors.toList())
 
+        // Filter by content (could be multiline)
+        val filteredByLineTextFileMarks = filteredByLineNumberFileMarks.stream()
+                .filter { filterByContent(it, document, lineNumber) }
+                .collect(Collectors.toList())
+
         // Deduplicate using exception text
-        return filteredByLineNumberFileMarks.distinctBy { it.text }
+        return filteredByLineTextFileMarks.distinctBy { it.text }
+    }
+
+    private fun filterByContent(fileMark: PyFileMark, document: Document, lineNumber: Int): Boolean {
+        val fileMarkContent = fileMark.checkContent.trim()
+
+        val lineStartOffset = document.getLineStartOffset(fileMark.beginLine)
+
+        val lineElementOffset = StringUtil.indexOf(document.immutableCharSequence,
+                fileMarkContent as CharSequence, lineStartOffset)
+
+        val maxOffset = document.getLineEndOffset(document.lineCount - 1)
+        if (lineElementOffset < 0 && lineElementOffset + fileMarkContent.length <= maxOffset) {
+            return false
+        }
+
+        val documentContent = document.getText(TextRange(
+                document.getLineStartOffset(lineNumber),
+                lineElementOffset + fileMarkContent.length))
+
+        return fileMarkContent == documentContent.trim()
     }
 }
---
+++ io.pyri/exampleprojects/multiline/test_m.py
@@ -0,0 +1,12 @@
+
+
+def multi():
+    some_list = ['element1']
+    assert some_list == [
+        'element1',
+        'element2',
+        'element3'
+    ]
+
+def test_multi():
+    multi()
