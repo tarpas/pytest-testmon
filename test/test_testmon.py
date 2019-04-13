tarpas/pytest-testmon

commit 2f7a9e8ddc34ad66620358b561d2b59f46e5dc55

Sqlalchemy has an internal pytest plugin which manipulates items in its pytest_collection_modifyitems, we want to come afterwards to be compatible.

testmon/pytest_testmon.py
@@ -183,6 +183,7 @@ class TestmonDeselect(object):
             self.collection_ignored.update(self.testmon_data.f_tests[strpath])
             return True
 
+    @pytest.mark.trylast
     def pytest_collection_modifyitems(self, session, config, items):
         self.testmon_data.collect_garbage(retain=self.collection_ignored.union(set([item.nodeid for item in items])))
 

commit 38d4c81559f90ffd3ecd7737249d5f4526792250

Do not garbage-collect items deselected, e.g. via "-k"
This regressed in 2f7a9e8.

testmon/pytest_testmon.py
@@ -185,9 +185,11 @@ class TestmonDeselect(object):
             self.collection_ignored.update(self.testmon_data.f_tests[strpath])
             return True
 
-    @pytest.mark.trylast
+    @pytest.hookimpl(hookwrapper=True, tryfirst=True)
     def pytest_collection_modifyitems(self, session, config, items):
         self.testmon_data.collect_garbage(retain=self.collection_ignored.union(set([item.nodeid for item in items])))
+
+        yield
 
         for item in items:
             assert item.nodeid not in self.collection_ignored, (item.nodeid, self.collection_ignored)

test/test_testmon.py
@@ -807,6 +807,39 @@ def test_add():
         assert result.reprec.countoutcomes() == [0, 0, 1]
         result.stdout.fnmatch_lines(["*1 error*", ])
 
+    def test_with_deselected(self, testdir):
+        testdir.makepyfile(test_a="""
+            import pytest
+
+            def test_fail():
+                assert 0
+
+            def test_pass():
+                pass
+        """)
+        result = testdir.runpytest_inprocess("--testmon")
+        assert result.reprec.countoutcomes() == [1, 0, 1]
+        result.stdout.fnmatch_lines(["*= 1 failed, 1 passed in*", ])
+
+        # Change test_fail.
+        testdir.makepyfile(test_a="""
+            import pytest
+
+            def test_fail():
+                assert 0, "changed"
+
+            def test_pass():
+                pass
+        """)
+        result = testdir.runpytest_inprocess("--testmon", "-s", "-k", "fail")
+        assert result.reprec.countoutcomes() == [0, 0, 1]
+        result.stdout.fnmatch_lines(["*= 1 failed, 1 deselected in*", ])
+
+        # Testmon should not have forgotten about test_pass.
+        result = testdir.runpytest_inprocess("--testmon", "-s")
+        assert result.reprec.countoutcomes() == [0, 0, 1]
+        result.stdout.fnmatch_lines(["*= 1 failed, 2 deselected in*", ])
+
 
 class TestXdist(object):
 