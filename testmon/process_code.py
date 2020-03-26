d319ca7ba853e4256bc9ad371f8fe61ae02205a7
Carson Gee <carson.gee@datarobot.com>
2020-03-26 10:37:39 -0700

    Backported reporting from v0.9.x branch

--- testmon/process_code.py
+++ testmon/process_code.py
@@ -17,7 +17,6 @@ def encode_lines(lines):
     return checksums
 
 
-
 GAP_MARKS = {i: "{}GAP".format(i) for i in range(-1, 64)}
 INVERTED_GAP_MARKS_CHECKSUMS = {encode_lines(["{}GAP".format(i)])[0]: i for i in range(-1, 64)}
 
